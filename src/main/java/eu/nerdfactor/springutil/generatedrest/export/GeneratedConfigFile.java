36aa11c28b480600bb666200d121a9bb49089f57
Daniel Klug <mail@nerdfactor.eu>
2024-01-06 18:25:42 +0100

    refactor: move GeneratedConfigFile out of YamlConfigExporter.

---
+++ src/main/java/eu/nerdfactor/springutil/generatedrest/export/GeneratedConfigFile.java
@@ -0,0 +1,14 @@
+package eu.nerdfactor.springutil.generatedrest.export;
+
+import eu.nerdfactor.springutil.generatedrest.config.ControllerConfiguration;
+
+import java.util.Map;
+
+public class GeneratedConfigFile {
+
+	public Map<String, String> config;
+	public Map<String, ControllerConfiguration> controllers;
+
+	public GeneratedConfigFile() {
+	}
+}
